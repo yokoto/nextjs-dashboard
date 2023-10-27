edfa7d0f24eb984287123294ed27a9c9386aed6e
yokoto <yo44ko55@gmail.com>
2023-10-28 01:20:49 +0900

    Add the mobile hero image

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,13 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
           />{" "}
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
